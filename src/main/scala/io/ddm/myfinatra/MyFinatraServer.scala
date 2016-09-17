f7ff23f3c10a2fcae7b57796b53d767028677d0b
Daniele Di Minica <d.diminica@resi.it>
2016-09-17 12:41:34 +0200

    Simplified

--- src/main/scala/io/ddm/myfinatra/MyFinatraServer.scala
+++ src/main/scala/io/ddm/myfinatra/MyFinatraServer.scala
@@ -8,7 +8,7 @@ package io.ddm.myfinatra
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.CommonFilters
 import com.twitter.finatra.http.routing.HttpRouter
-import io.ddm.myfinatra.controllers.{StatsController, WeightResource}
+import io.ddm.myfinatra.controllers.{StatsController}
 
 object MyFinatraApp extends MyFinatraServer
 
@@ -19,12 +19,9 @@ class MyFinatraServer extends HttpServer {
   override protected def defaultHttpServerName: String = "MyFitMan"
 
   override protected def configureHttp(router: HttpRouter): Unit = {
-    router.add(new StatsController)
-    router.add(new WeightResource)
-//    router
-//      .filter[CommonFilters]
-//      .add[StatsController]
-//      .add[WeightResource]
+    router
+      .filter[CommonFilters]
+      .add[StatsController]
 
   }
 
--- src/main/scala/io/ddm/myfinatra/controllers/Controllers.scala
+++ src/main/scala/io/ddm/myfinatra/controllers/Controllers.scala
@@ -11,14 +11,6 @@ import scala.collection.mutable
   * Created by white on 16/09/16.
   */
 
-case class Weight(
-                   user: String,
-                   weight: Int,
-                   status: Option[String],
-                   postedAt: Instant = Instant.now()
-                 )
-
-
 case class Stat(
                index: String,
                data: Int
@@ -28,39 +20,9 @@ class StatsController extends Controller {
 
   val db = mutable.ListBuffer[Stat]()
   db += Stat(index = "index1", data = 1)
-  db += Stat(index = "index2", data = 2)
-  db += Stat(index = "index3", data = 3)
-  db += Stat(index = "index4", data = 4)
-  db += Stat(index = "index5", data = 5)
 
   get("/stats") { request: Request =>
     db
   }
 }
 
-class WeightResource extends Controller {
-
-  val db = mutable.Map[String, List[Weight]]()
-
-  get("/weights") { request: Request =>
-    info("finding all weights for all users...")
-    db
-  }
-
-  get("/weights/:user") { request: Request =>
-    info( s"""finding weight for user ${request.params("user")}""")
-    db.getOrElse(request.params("user"), List())
-  }
-
-  post("/weights") { weight: Weight =>
-    val r = time(s"Total time take to post weight for user '${weight.user}' is %d ms") {
-      val weightsForUser = db.get(weight.user) match {
-        case Some(weights) => weights :+ weight
-        case None => List(weight)
-      }
-      db.put(weight.user, weightsForUser)
-      response.created.location(s"/weights/${weight.user}")
-    }
-    r
-  }
-}
